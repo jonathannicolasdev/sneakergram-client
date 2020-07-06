681478df113f45eea200b30e149fd9e7c28b0e79
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-07-05 20:27:14 -0700

    Add html for Register page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { useState } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Home from './pages/Home'
-import RegisterLogin from './pages/RegisterLogin'
+import Home from "./pages/Home";
+import RegisterLogin from "./pages/RegisterLogin";
 
 export default function App() {
-  const [authenticated, setAuthenticated] = useState(false)
+  const [authenticated, setAuthenticated] = useState(false);
 
   return (
     <Router>
       <div>
         <Switch>
-          <Route path='/'>{authenticated ? <Home /> : <RegisterLogin />}</Route>
+          <Route path="/">{authenticated ? <Home /> : <RegisterLogin />}</Route>
         </Switch>
 
-        <button onClick={() => setAuthenticated(!authenticated)}>
-          Change Auth
-        </button>
-
-        <p>{JSON.stringify(authenticated)}</p>
+        <footer>
+          <button onClick={() => setAuthenticated(!authenticated)}>
+            Change Auth
+          </button>
+        </footer>
       </div>
     </Router>
-  )
+  );
 }
--- src/pages/RegisterLogin.js
+++ src/pages/RegisterLogin.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function RegisterLogin() {
   return (
     <div>
-      <h1>Register/Login</h1>
-
-      <form>
-        <input type='text' />
-      </form>
+      <div>
+        <img src="/assets/featured-image.jpg" alt="featured" />
+      </div>
+      <div>
+        <h1>Sneakergram</h1>
+        <form>
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="submit" value="Register" />
+        </form>
+        <div>
+          <p>
+            Have an account? <a href="/login">Login</a>
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
