0ea9686df1d14a31c6619d7b0b5f85ca190a4473
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-07-05 20:45:38 -0700

    Add emotion and container styling

--- src/pages/RegisterLogin.js
+++ src/pages/RegisterLogin.js
@@ -1,25 +1,35 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
 export default function RegisterLogin() {
   return (
     <div>
-      <div>
-        <img src="/assets/featured-image.jpg" alt="featured" />
-      </div>
-      <div>
-        <h1>Sneakergram</h1>
-        <form>
-          <input type="text" placeholder="Username" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="submit" value="Register" />
-        </form>
+      <Container>
         <div>
-          <p>
-            Have an account? <a href="/login">Login</a>
-          </p>
+          <img src="/assets/featured-image.jpg" alt="featured" />
         </div>
-      </div>
+        <div>
+          <h1>Sneakergram</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="submit" value="Register" />
+          </form>
+          <div>
+            <p>
+              Have an account? <a href="/login">Login</a>
+            </p>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 }
