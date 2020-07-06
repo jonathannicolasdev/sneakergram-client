2c0d3104ff25f39d40d660e367b2ba95f37f349e
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-07-05 21:38:17 -0700

    Finalize styling of register page

--- src/pages/RegisterLogin.js
+++ src/pages/RegisterLogin.js
@@ -12,6 +12,45 @@ const Logo = styled.h1`
   font-family: Comfortaa;
 `;
 
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  margin-bottom: 10px;
+  padding: 8px 10px;
+  background: #eeeeee;
+  border-radius: 2px;
+  border: none;
+`;
+
+const SubmitInput = styled.input`
+  padding: 8px 10px;
+  background: #66bd22;
+  border-radius: 2px;
+  border: none;
+  margin-top: 5px;
+  color: white;
+  font-size: 14px;
+  font-weight: 700;
+`;
+
+const Help = styled.div`
+  display: flex;
+  justify-content: center;
+  a {
+    color: #66bd22;
+    text-decoration: none;
+  }
+`;
+
+const Section = styled.div`
+  border: 1px solid #cccccc;
+  padding: 30px;
+  border-radius: 2px;
+`;
+
 export default function RegisterLogin() {
   return (
     <div>
@@ -19,20 +58,20 @@ export default function RegisterLogin() {
         <div>
           <img src="/assets/featured-image.jpg" alt="featured" />
         </div>
-        <div>
+        <Section>
           <Logo>Sneakergram</Logo>
-          <form>
-            <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="submit" value="Register" />
-          </form>
-          <div>
+          <Form>
+            <Input type="text" placeholder="Username" />
+            <Input type="email" placeholder="Email" />
+            <Input type="password" placeholder="Password" />
+            <SubmitInput type="submit" value="Register" />
+          </Form>
+          <Help>
             <p>
               Have an account? <a href="/login">Login</a>
             </p>
-          </div>
-        </div>
+          </Help>
+        </Section>
       </Container>
     </div>
   );
