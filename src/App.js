2747bb7f562b4245b2bc6b9bbb6f436ba1073a5e
M Haidar Hanif <git@mhaidarhanif.com>
2020-07-09 11:50:53 +0700

    Refactor Router into its own
    Retrieve state and dispatch from Redux through connect

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import React, { useState } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import React from 'react'
 import { Provider } from 'react-redux'
 
 import store from './store'
-
-import Home from './pages/Home'
-import RegisterLogin from './pages/RegisterLogin'
+import Router from './Router'
 
 export default function App() {
-  const [authenticated, setAuthenticated] = useState(false)
-
   return (
     <Provider store={store}>
-      <Router>
-        <div>
-          <Switch>
-            <Route path='/'>
-              {authenticated ? <Home /> : <RegisterLogin />}
-            </Route>
-          </Switch>
-
-          <footer>
-            <button onClick={() => setAuthenticated(!authenticated)}>
-              Change Auth
-            </button>
-          </footer>
-        </div>
-      </Router>
+      <Router />
     </Provider>
   )
 }
---
+++ src/Router.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import RegisterLogin from './pages/RegisterLogin'
+
+const Router = ({ authenticated, login, logout }) => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path='/'>{authenticated ? <Home /> : <RegisterLogin />}</Route>
+        </Switch>
+
+        <footer>
+          <button onClick={() => login()}>Login</button>
+          <button onClick={() => logout()}>Logout</button>
+        </footer>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  authenticated: state.authenticated,
+})
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    login: () => dispatch({ type: 'SET_AUTHENTICATED_TRUE' }),
+    logout: () => dispatch({ type: 'SET_AUTHENTICATED_FALSE' }),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Router)
