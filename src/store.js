0596496e8566f7a7d3d9e622c829b57172aaa0a3
M Haidar Hanif <git@mhaidarhanif.com>
2020-07-09 11:23:33 +0700

    Create redux store file

---
+++ src/store.js
@@ -0,0 +1,28 @@
+import { createStore } from 'redux'
+
+function reducer(
+  initialState = {
+    authenticated: false,
+  },
+  action
+) {
+  switch (action.type) {
+    case 'SET_AUTHENTICATED_TRUE':
+      return {
+        authenticated: true,
+      }
+    case 'SET_AUTHENTICATED_FALSE':
+      return {
+        authenticated: false,
+      }
+    default:
+      return initialState
+  }
+}
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+export default store
