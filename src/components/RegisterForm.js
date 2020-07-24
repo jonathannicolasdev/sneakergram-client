6698121fbac8184d5fd815dd58ada5ebcb75c2db
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-07-24 00:10:35 -0700

    Setup api url

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import styled from '@emotion/styled'
+import React from "react";
+import { useForm } from "react-hook-form";
+import styled from "@emotion/styled";
+import axios from "axios";
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
-`
+`;
 
 const Input = styled.input`
   margin-bottom: 10px;
@@ -13,7 +14,7 @@ const Input = styled.input`
   background: #eeeeee;
   border-radius: 2px;
   border: none;
-`
+`;
 
 const SubmitInput = styled.input`
   padding: 8px 10px;
@@ -24,32 +25,33 @@ const SubmitInput = styled.input`
   color: white;
   font-size: 14px;
   font-weight: 700;
-`
+`;
 
 const RegisterForm = () => {
-  const { register, handleSubmit } = useForm()
-  const onSubmit = (data) => {
-    console.log(data)
-  }
+  const { register, handleSubmit } = useForm();
+  const onSubmit = async (data) => {
+    console.log(data);
+    await axios.get(process.env.REACT_APP_API_URL);
+  };
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <Input
-        name='username'
-        type='text'
-        placeholder='Username'
+        name="username"
+        type="text"
+        placeholder="Username"
         ref={register}
       />
-      <Input name='email' type='email' placeholder='Email' ref={register} />
+      <Input name="email" type="email" placeholder="Email" ref={register} />
       <Input
-        name='password'
-        type='password'
-        placeholder='Password'
+        name="password"
+        type="password"
+        placeholder="Password"
         ref={register}
       />
-      <SubmitInput type='submit' value='Register' />
+      <SubmitInput type="submit" value="Register" />
     </Form>
-  )
-}
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
