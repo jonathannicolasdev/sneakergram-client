21294243d7cd10bfbc1cc05bd7cde09e5f9d2658
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-07-28 23:50:51 -0700

    Add login request

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import styled from '@emotion/styled'
-import { connect } from 'react-redux'
+import React from "react";
+import { useForm } from "react-hook-form";
+import styled from "@emotion/styled";
+import { connect } from "react-redux";
+import axios from "axios";
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
-`
+`;
 
 const Input = styled.input`
   margin-bottom: 10px;
@@ -14,7 +15,7 @@ const Input = styled.input`
   background: #eeeeee;
   border-radius: 2px;
   border: none;
-`
+`;
 
 const SubmitInput = styled.input`
   padding: 8px 10px;
@@ -25,37 +26,46 @@ const SubmitInput = styled.input`
   color: white;
   font-size: 14px;
   font-weight: 700;
-`
+`;
 
 const LoginForm = ({ authenticated, login }) => {
-  const { register, handleSubmit } = useForm()
-  const onSubmit = (data) => {
-    console.log(data)
-    login()
-  }
+  const { register, handleSubmit } = useForm();
+  const onSubmit = async (data) => {
+    console.log(data);
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/users/login`,
+        data
+      );
+      console.log(response);
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
-      <Input name='email' type='email' placeholder='Email' ref={register} />
+      <Input name="email" type="email" placeholder="Email" ref={register} />
       <Input
-        name='password'
-        type='password'
-        placeholder='Password'
+        name="password"
+        type="password"
+        placeholder="Password"
         ref={register}
       />
-      <SubmitInput type='submit' value='Login' />
+      <SubmitInput type="submit" value="Login" />
     </Form>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => ({
   authenticated: state.authenticated,
-})
+});
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    login: () => dispatch({ type: 'SET_AUTHENTICATED_TRUE' }),
-  }
-}
+    login: () => dispatch({ type: "SET_AUTHENTICATED_TRUE" }),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(LoginForm)
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
