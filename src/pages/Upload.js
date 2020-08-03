9d0ab272d798d4b80deb156cadd12cbb865cfe99
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-08-02 23:17:57 -0700

    Try to sent request with multipart

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -4,12 +4,32 @@ import { useForm } from "react-hook-form";
 
 function Upload() {
   const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
-  console.log(watch("sneaker")); // watch input value by passing the name of it
+  const onSubmit = async (data) => {
+    try {
+      console.log(data);
+      let bodyFormData = new FormData();
+      bodyFormData.append("sneaker", data.sneaker[0]);
+
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/sneakers`,
+        { data: bodyFormData },
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // console.log(watch("sneaker")); // watch input value by passing the name of it
 
   return (
     <div>
-      <form encType="multipart/form-data" onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <input name="sneaker" ref={register} type="file" />
           <input type="submit" value="Upload Sneaker" />
