b86a39c665db3733fcfd66a5b5c8473d4cbb47e9
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-08-12 22:54:45 -0700

    Add style and state to sneaker add form

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,19 +1,85 @@
 import React, { useState } from "react";
 import axios from "axios";
+import styled from "@emotion/styled";
+
+const Form = styled.form`
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Field = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`;
+
+const Label = styled.label`
+  font-weight: bold;
+  font-size: 1.2em;
+  margin-bottom: 2px;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  font-size: 1.2em;
+  border: 2px;
+  border-radius: 5px;
+  border-color: border;
+`;
+
+const Submit = styled.input`
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  color: #fff;
+  font-size: 1.2em;
+  font-weight: bold;
+  padding: 10px;
+  width: 100%;
+  transition: background-color 0.2s ease-in-out;
+  background-color: blue;
+  &:hover {
+    background-color: darkblue;
+  }
+`;
 
 const Upload = () => {
   const [state, setState] = useState({
+    name: "",
+    style: "",
+    colorway: "",
+    retailPrice: 0,
+    releaseDate: "",
+    size: "",
+    location: "",
     file: null,
   });
 
   const onFormSubmit = async (event) => {
     event.preventDefault();
-    await fileUpload(state.file);
+
+    console.log(state);
+    // await fileUpload(state.file);
   };
 
-  const onChange = (event) => {
+  const onFileChange = (event) => {
     setState({
+      ...state,
       file: event.target.files[0],
+    });
+  };
+
+  const onTextChange = (event) => {
+    const name = event.target.name;
+    let value = event.target.value;
+
+    if (event.target.name === "retailPrice") {
+      value = Number(value);
+    }
+
+    setState({
+      ...state,
+      [name]: value,
     });
   };
 
@@ -39,52 +105,74 @@ const Upload = () => {
   };
 
   return (
-    <form onSubmit={onFormSubmit}>
-      <h1>File Upload</h1>
-      <input type="file" onChange={onChange} />
-      <div>
-        <label>Name</label>
-        <input
+    <Form onSubmit={onFormSubmit}>
+      <h1>Post New Sneaker</h1>
+      <Field>
+        <Input type="file" onChange={onFileChange} />
+      </Field>
+      <Field>
+        <Label>Name</Label>
+        <Input
           name="name"
           type="text"
           placeholder="Jordan 1 Retro High Hot Punch (W)"
+          onChange={onTextChange}
         />
-      </div>
-      <div>
-        <label>Style</label>
-        <input name="style" type="text" placeholder="AQ9131-600" />
-      </div>
-      <div>
-        <label>Colorway</label>
-        <input name="colorway" type="text" placeholder="HOT PUNCH/VOLT-BLACK" />
-      </div>
-      <div>
-        <label>Retail Price (USD)</label>
-        <input name="retail-price" type="number" placeholder="145" />
-      </div>
-      <div>
-        <label>Release Date</label>
-        <input name="release-date" type="date" />
-      </div>
-      <div>
-        <label>Size</label>
-        <input name="size" type="text" placeholder="9.5 US" />
-      </div>
-      <div>
-        <label>Location</label>
-        <input name="location" type="text" placeholder="Jakarta, Indonesia" />
-      </div>
-      <button type="submit">Post Sneaker</button>
-    </form>
+      </Field>
+      <Field>
+        <Label>Style</Label>
+        <Input
+          name="style"
+          type="text"
+          placeholder="AQ9131-600"
+          onChange={onTextChange}
+        />
+      </Field>
+      <Field>
+        <Label>Colorway</Label>
+        <Input
+          name="colorway"
+          type="text"
+          placeholder="HOT PUNCH/VOLT-BLACK"
+          onChange={onTextChange}
+        />
+      </Field>
+      <Field>
+        <Label>Retail Price (USD)</Label>
+        <Input
+          name="retailPrice"
+          type="number"
+          placeholder="145"
+          onChange={onTextChange}
+        />
+      </Field>
+      <Field>
+        <Label>Release Date</Label>
+        <Input name="releaseDate" type="date" onChange={onTextChange} />
+      </Field>
+      <Field>
+        <Label>Size</Label>
+        <Input
+          name="size"
+          type="text"
+          placeholder="9.5 US"
+          onChange={onTextChange}
+        />
+      </Field>
+      <Field>
+        <Label>Location</Label>
+        <Input
+          name="location"
+          type="text"
+          placeholder="Jakarta, Indonesia"
+          onChange={onTextChange}
+        />
+      </Field>
+      <Field>
+        <Submit type="submit" value="Post Sneaker" />
+      </Field>
+    </Form>
   );
 };
 
 export default Upload;
-
-//  name: "dummy",
-//         style: "dummy",
-//         colorway: "dummy",
-//         retailPrice: 100,
-//         releaseDate: "2020-01-01",
-//         size: "10",
-//         location: "Canada",
