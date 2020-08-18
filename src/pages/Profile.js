4b1c0699b059ca6466bce5aa78c81bac65f2be3e
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-08-18 09:31:53 -0700

    Add user display on profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,46 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import Header from "../components/Header";
+import styled from "@emotion/styled";
+
+const User = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const UserImage = styled.img`
+  border-radius: 50%;
+  width: 80px;
+`;
+
+const UserInfo = styled.div`
+  margin: 15px;
+  h1 {
+    font-size: 16px;
+  }
+  h2 {
+    font-size: 12px;
+  }
+`;
 
 const Profile = () => {
   let { username } = useParams();
 
-  return <div>{username}</div>;
+  return (
+    <div>
+      <Header />
+      <User>
+        <UserImage src="/assets/avatars/jonathan.jpg"></UserImage>
+        <UserInfo>
+          <h1>{username}</h1>
+          <h2>Jonathan Nicolas</h2>
+          <p>8 sneakers</p>
+          <p>with a sneaker he astonished paris</p>
+        </UserInfo>
+      </User>
+    </div>
+  );
 };
 
 export default Profile;
