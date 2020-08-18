5c828535de19493c475de2cb2fb53299445b878e
M Haidar Hanif <git@mhaidarhanif.com>
2020-08-19 00:14:26 +0700

    Reconfigure Redux store w/ thunk & redux-devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,17 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
 import { loadState, saveState } from "../utils/storage";
 
 import rootReducer from "./reducers";
 
+const middlewares = [thunk];
+
 const store = createStore(
   rootReducer,
   loadState(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(applyMiddleware(...middlewares))
 );
 
 saveState(store.getState());
