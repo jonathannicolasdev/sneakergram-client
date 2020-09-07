ea9661d715a147c836a3019a2f14777b0d1a87b6
M Haidar Hanif <git@mhaidarhanif.com>
2020-09-07 11:58:38 +0700

    Log API_URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import Router from "./Router";
 
+console.log(process.env.REACT_APP_API_URL);
+
 export default function App() {
   return (
     <Provider store={store}>
