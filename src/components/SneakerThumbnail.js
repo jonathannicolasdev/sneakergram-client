692fe68f8ac8f984eeeae068e0f7a98581077d9e
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-09-07 22:14:35 -0700

    Add modal overlay to sneakerThumbnail

--- src/components/SneakerThumbnail.js
+++ src/components/SneakerThumbnail.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
+import Modal from "react-overlays/Modal";
+import SneakerCard from "./SneakerCard";
 
 const Thumbnail = styled.div`
   display: flex;
@@ -12,6 +14,26 @@ const Thumbnail = styled.div`
   overflow: hidden;
 `;
 
+const Backdrop = styled("div")`
+  position: fixed;
+  z-index: 1040;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #000;
+  opacity: 0.5;
+`;
+
+// we use some pseudo random coords so nested modals
+// don't sit right on top of each other.
+const RandomlyPositionedModal = styled(Modal)`
+  position: fixed;
+  z-index: 1040;
+  top: 20%;
+  left: 25%;
+`;
+
 const SneakerImage = styled.img`
   width: 300px;
   /* height: 200px; */
@@ -19,9 +41,21 @@ const SneakerImage = styled.img`
 `;
 
 const SneakerThumbnail = ({ sneaker }) => {
+  const [show, setShow] = useState(false);
+
+  const renderBackdrop = (props) => <Backdrop {...props} />;
   return (
-    <Thumbnail>
+    <Thumbnail onClick={() => setShow(true)}>
       <SneakerImage src={sneaker.imageUrl} alt={sneaker.name} />
+
+      <RandomlyPositionedModal
+        show={show}
+        onHide={() => setShow(false)}
+        renderBackdrop={renderBackdrop}
+        aria-labelledby="modal-label"
+      >
+        <SneakerCard sneaker={sneaker}></SneakerCard>
+      </RandomlyPositionedModal>
     </Thumbnail>
   );
 };
